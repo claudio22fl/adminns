e35659e3adb589276189271d01cb0b23f14e9d05
claudio22fl <claudio22fl@gmail.com>
2023-07-10 13:36:25 -0400

    update search

--- app/component/grafic/GraficCpu.tsx
+++ app/component/grafic/GraficCpu.tsx
@@ -12,39 +12,46 @@ export default function GraficCpu({ datos }: any) {
       </div>
     );
   }
+  try {
+    const splitdata = datos.split(" ");
+    const splitDataFinal = splitdata.filter((servers: string) => servers != "");
 
-  const splitdata = datos.split(" ");
-  const splitDataFinal = splitdata.filter((servers: string) => servers != "");
+    const data = [
+      {
+        name: "En uso",
+        value: roundDecimal(Number(splitDataFinal[22]), 2),
+      },
+      {
+        name: "Libre",
+        value: roundDecimal(Number(splitDataFinal[31].slice(0, 4)), 2),
+      },
+    ];
 
-  const data = [
-    {
-      name: "En uso",
-      value: roundDecimal(Number(splitDataFinal[22]), 2),
-    },
-    {
-      name: "Libre",
-      value: roundDecimal(Number(splitDataFinal[31].slice(0, 4)), 2),
-    },
-  ];
-
-  return (
-    <PieChart width={300} height={300}>
-      <Pie
-        data={data}
-        dataKey="value"
-        nameKey="name"
-        cx="50%"
-        cy="50%"
-        outerRadius={80}
-        fill="#8884d8"
-        label
-      >
-        {data.map((_entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip formatter={(value) => `%${value}`} />
-      <Legend />
-    </PieChart>
-  );
+    return (
+      <PieChart width={300} height={300}>
+        <Pie
+          data={data}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill="#8884d8"
+          label
+        >
+          {data.map((_entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip formatter={(value) => `%${value}`} />
+        <Legend />
+      </PieChart>
+    );
+  } catch {
+    return (
+      <div className="text-center w-72 top-20 p-24">
+        <Isloading />
+      </div>
+    );
+  }
 }
--- app/component/tableComponent/useSearchData.tsx
+++ app/component/tableComponent/useSearchData.tsx
@@ -26,7 +26,8 @@ export const useSearchData = (posts: ISim[]) => {
           numerosimMatch ||
           simMatch ||
           clienteMatch ||
-          correoMatch
+          correoMatch ||
+          empresaMatch
         );
       })
     );
